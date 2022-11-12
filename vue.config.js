301406a3cc46a7290093f48bd2efa9902f7737f4
Cezar Almeida <cezar.almeidajr@gmail.com>
2022-11-12 01:55:06 -0300

    fixing build errors

--- vue.config.js
+++ vue.config.js
@@ -35,4 +35,21 @@ module.exports = {
       },
     },
   },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        "stream": require.resolve("stream-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "zlib": require.resolve("browserify-zlib"),
+        "path": require.resolve("path-browserify"),
+        "os": require.resolve("os-browserify"),
+        "child_process": false,
+        "fs": false,
+        "net": false,
+        "tls": false,
+      }
+    }
+  },
 }
